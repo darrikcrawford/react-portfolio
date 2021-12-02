eda7ead8e4c22ec34f80487f5d1c0403010385d2
Darrik Crawford <darrikcrawford@yahoo.com>
2021-12-01 23:02:05 -0600

    update about me

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import coverImage from "../../assets/images/hero-image.jpg";
+import profileImage from "../../assets/images/pProfile-picture.jpg";
 
 function About() {
     return (
-      <section className="my-5">
-        <h1 id="about">Who am I?</h1>
-        <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+      <section className="AboutMe">
+        <img className="align-self-center" alt="Darrik in a suit" src={profileImage}/>
+        <p className="about align-self-center">Hi, I’m Darrik Crawford. I am a full-stack developer living in Austin, TX and and currently work at Indeed.com as a Senior Account Executive. Working at Indeed has exposed me to many different technical roles including SWEs, Developers and more. Solving problems has always excited me, so I started to lean into this coding path. I’m excited for this next journey. Please reach out with any questions or comments.</p>
       </section>
     );
 }
